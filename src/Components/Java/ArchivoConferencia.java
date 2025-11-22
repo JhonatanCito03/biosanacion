051c41c450ddc297e454e5aa894b96c473081415
alejo <alejoperez161006@hotmail.com>
2025-11-21 22:28:27 -0500

    java

--- src/Components/Java/ArchivoConferencia.java
+++ src/Components/Java/ArchivoConferencia.java
@@ -1,7 +1,12 @@
+import servlet.ServletException;
+import servlet.annotation.WebServlet;import
 import java.io.*;
 import java.util.*;
-import java.time.*;
 
 public class ArchivoConferencia {
     static Scanner sc = new Scanner(System.in);
+
+    public static void main(String[] args) {
+    }
+
 }
